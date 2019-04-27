2a2df09213937374fc059c5971caa270aac128ea
Exhidor <jipay.detiege@gmail.com>
2019-04-27 15:37:35 +0200

    add sprite robot

---
+++ Assets/Scripts/PlayerRobot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRobot : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        int direction = 0;
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+
+        if(direction != 0)
+        {
+            float xMove = speed * direction * Time.deltaTime;
+            Vector3 move = new Vector3(xMove, 0, 0);
+
+            transform.position += move;
+        }
+    }
+}
